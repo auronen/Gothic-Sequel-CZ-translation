3172f40e1e87dcaa979a347fb2621d04a2bcb3c5
Auronen <HThiele@seznam.cz>
2021-05-06 00:08:13 +0200

    překlad EV_Phoenix

--- content/Story/Events/EV_Phoenix.d
+++ content/Story/Events/EV_Phoenix.d
@@ -45,9 +45,11 @@ func void EV_BOOK_PHOENIX_S1 ()
 	Doc_PrintLine	( nDocID,  0, "-------------------"			);
 	Doc_SetFont 	( nDocID, -1, FONT_BOOK_SMALL	 		); 	// -1 -> all pages
 	Doc_PrintLine	( nDocID,  0, "");
-	Doc_PrintLines	( nDocID,  0, "Du hast das Rätsel des Phoenix entschlüsselt. Du bist würdig, das Wissen des Feuers an dich zu nehmen.");
+//	Doc_PrintLines	( nDocID,  0, "Du hast das Rätsel des Phoenix entschlüsselt. Du bist würdig, das Wissen des Feuers an dich zu nehmen.");
+	Doc_PrintLines	( nDocID,  0, "Rozluštil jsi hádanku fénixe. Jsi hoden přijmout poznání Ohně.");
 	Doc_PrintLine	( nDocID,  0, "");
-	Doc_PrintLines	( nDocID,  0, "Bewahre das Wissen und vergeude es nicht sinnlos. Dies ist der letzte Hort des Feuers innerhalb der Barriere.");
+//	Doc_PrintLines	( nDocID,  0, "Bewahre das Wissen und vergeude es nicht sinnlos. Dies ist der letzte Hort des Feuers innerhalb der Barriere.");
+	Doc_PrintLines	( nDocID,  0, "Chovej jej v bezpečí a zbytečně jím nemrhej. Je to poslední útočiště Ohně v Bariéře.");
 
 
 	//2.Seite
@@ -61,13 +63,15 @@ func void EV_BOOK_PHOENIX_S1 ()
 	Doc_PrintLine	( nDocID,  1, "");
 	Doc_PrintLine	( nDocID,  1, "");
 	Doc_PrintLine	( nDocID,  1, "");
-	Doc_PrintLines	( nDocID,  1, "Der Phoenix hat sich aus seiner Asche erhoben, und die Macht des Feuers erstrahlt in neuem Glanz. Dies ist der Kreislauf des Lebens. Es war immer so. Es wird immer so sein.");
+//	Doc_PrintLines	( nDocID,  1, "Der Phoenix hat sich aus seiner Asche erhoben, und die Macht des Feuers erstrahlt in neuem Glanz. Dies ist der Kreislauf des Lebens. Es war immer so. Es wird immer so sein.");
+	Doc_PrintLines	( nDocID,  1, "Fénix se znovu zrodil z popela a jeho mocný Oheň třpytivě zaří. To je kolběh života. Tak to vždy bylo a tak to vžy bude.");
 	Doc_Show		( nDocID );
 
 	if	(Phoenix_State < 9)
 	{
 			B_GiveXP			(XP_Phoenix9);
-			B_LogEntry			(CH1_Phoenix, "Ich habe die Geheime Kammer der Feuermagier gefunden. Doch Corristo warnte im letzten Buch davor, das Wissen zu verschwenden, denn es sei 'der letzte Hort des Feuers innerhalb der Barriere'.");
+//			B_LogEntry			(CH1_Phoenix, "Ich habe die Geheime Kammer der Feuermagier gefunden. Doch Corristo warnte im letzten Buch davor, das Wissen zu verschwenden, denn es sei 'der letzte Hort des Feuers innerhalb der Barriere'.");
+			B_LogEntry			(CH1_Phoenix, "Našel jsem tajnou komnatu Mágů ohně. Ale Corristo varoval v poslední knize před mrháním vědomostmi, neboť jsou 'poslední útočiště Ohně v Bariéře'.");
 			Log_SetTopicStatus	(CH1_Phoenix, LOG_SUCCESS);
 			Phoenix_State = 9;
 	};
