ef0e513e6e5b0bd7544319c3210c3038d858f156
Auronen <HThiele@seznam.cz>
2021-05-30 15:34:57 +0200

    překlad a oprava

--- content/Story/B/b_buildalchemierecipestring.d
+++ content/Story/B/b_buildalchemierecipestring.d
@@ -3,7 +3,10 @@ func string B_BuildAlchemieRecipeString ( 	var string 	string_prefix, var string
 	var string string_recipe;
 
 //	string_recipe = ConcatStrings (string_prefix, "  -=Extrakt=- ");
-	string_recipe = ConcatStrings (string_prefix, "\nESENCE ");
+//	string_recipe = ConcatStrings (string_prefix, "\nESENCE ");
+	string_recipe = ConcatStrings (string_prefix, "\n");
+	string_recipe = ConcatStrings (string_recipe, NAME_Essence);
+	string_recipe = ConcatStrings (string_recipe, " ");
 	string_recipe = ConcatStrings (string_recipe, "1x ");
 	string_recipe = ConcatStrings (string_recipe, regName);
 	string_recipe = ConcatStrings (string_recipe, ", 1x ");
@@ -12,7 +15,10 @@ func string B_BuildAlchemieRecipeString ( 	var string 	string_prefix, var string
 	string_recipe = ConcatStrings (string_recipe, NAME_Alcohol);
 
 //	string_recipe = ConcatStrings (string_recipe, ".  -=Extrakt=- ");
-	string_recipe = ConcatStrings (string_recipe, "\nEXTRAKT ");
+//	string_recipe = ConcatStrings (string_recipe, "\nEXTRAKT ");
+	string_recipe = ConcatStrings (string_prefix, "\n");
+	string_recipe = ConcatStrings (string_recipe, NAME_Extract);
+	string_recipe = ConcatStrings (string_recipe, " ");
 	string_recipe = ConcatStrings (string_recipe, "1x ");
 	string_recipe = ConcatStrings (string_recipe, regName);
 	string_recipe = ConcatStrings (string_recipe, ", 1x ");
@@ -21,7 +27,10 @@ func string B_BuildAlchemieRecipeString ( 	var string 	string_prefix, var string
 	string_recipe = ConcatStrings (string_recipe, NAME_Alcohol);
 
 //	string_recipe = ConcatStrings (string_recipe, ".  -=Elixier=- ");
-	string_recipe = ConcatStrings (string_recipe, "\nELIXÍR ");
+//	string_recipe = ConcatStrings (string_recipe, "\nELIXÍR ");
+	string_recipe = ConcatStrings (string_prefix, "\n");
+	string_recipe = ConcatStrings (string_recipe, NAME_Elixir);
+	string_recipe = ConcatStrings (string_recipe, " ");
 	string_recipe = ConcatStrings (string_recipe, "1x ");
 	string_recipe = ConcatStrings (string_recipe, regName);
 	string_recipe = ConcatStrings (string_recipe, ", 1x ");
