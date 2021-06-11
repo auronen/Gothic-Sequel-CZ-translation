15b3bb7ca78b34500f733fedc3103e679a5de058
ZerculePoirot <83756680+ZerculePoirot@users.noreply.github.com>
2021-06-11 20:00:19 +0200

    Update DIA_mil_129_militia.d
    Překlad.

--- content/Story/MISSIONS/DIA_mil_129_militia.d
+++ content/Story/MISSIONS/DIA_mil_129_militia.d
@@ -29,10 +29,14 @@ func int MIL_129_Militia_STOP_Condition ()
 };
 func void MIL_129_Militia_STOP_Info ()
 {
-	AI_Output			(self, hero, "MIL_129_STOP_00_01"); //HALT! Du betrittst jetzt das Viertel der Kranken!
-	AI_Output			(hero, self, "MIL_129_STOP_15_02"); //Ja und?
-	AI_Output			(self, hero, "MIL_129_STOP_00_03"); //Niemand weiß, was es mit der Krankheit auf sich hat. Besser du hälst dich fern von diesen Gestalten!
-	AI_Output			(hero, self, "MIL_129_STOP_15_04"); //Ich bin vorsichtig.
+//	AI_Output			(self, hero, "MIL_129_STOP_00_01"); //HALT! Du betrittst jetzt das Viertel der Kranken!
+	AI_Output			(self, hero, "MIL_129_STOP_00_01"); //STÁT! Vstupuješ do čtvrti nemocných!
+//	AI_Output			(hero, self, "MIL_129_STOP_15_02"); //Ja und?
+	AI_Output			(hero, self, "MIL_129_STOP_15_02"); //No a?
+//	AI_Output			(self, hero, "MIL_129_STOP_00_03"); //Niemand weiß, was es mit der Krankheit auf sich hat. Besser du hälst dich fern von diesen Gestalten!
+	AI_Output			(self, hero, "MIL_129_STOP_00_03"); //Nikdo neví, co je to za nemoc. Raději se odsud drž dále!
+//	AI_Output			(hero, self, "MIL_129_STOP_15_04"); //Ich bin vorsichtig.
+	AI_Output			(hero, self, "MIL_129_STOP_15_04"); //Já jsem opatrný.	
 	AI_StopProcessInfos	(self);
 };
 
